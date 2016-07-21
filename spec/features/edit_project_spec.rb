dc5fa0d4c3ad62bb3a8ce5fdb266d0358f8c3b2d
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-07-21 21:00:34 +0200

    added rspec and capybara and edit_project_spec

---
+++ spec/features/edit_project_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Editing a project" do
+
+    it "updates the project and shows the project's updated details" do
+      project = Project.create( name: "abn",
+                                description: "We are going to create the next big thing",
+                                target_pledge_amount: 100000,
+                                website: "www.zekerweten.nl",
+                                pledging_ends_on: "2016-08-13")
+
+      visit project_url(project)
+
+      click_link 'Edit'
+
+      expect(current_path).to eq(edit_project_path(project))
+
+      expect(find_field('Name').value).to eq(project.name)
+    end
+
+end
