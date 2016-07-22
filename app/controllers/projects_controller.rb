a9cbefce91cd55669f900ba4da95514711d1b6fe
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-07-22 13:22:02 +0200

    added new project link in index, new and create actions in projectcontroller and create_project_spec

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,9 +13,23 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-    project_params = params.require(:project).permit(:name, :description, :target_pledge_amount, :website, :pledging_ends_on)
     @project.update(project_params)
     redirect_to @project
+  end
+
+  def new
+    @project = Project.new(params[:project])
+  end
+
+  def create
+    @project = Project.create(project_params)
+    redirect_to @project
+  end
+
+private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :target_pledge_amount, :website, :pledging_ends_on)
   end
 
 end
---
+++ spec/features/create_project_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Creating a new project" do
+  it "saves the project and shows the new project's details" do
+    visit projects_url
+
+    click_link 'Add New Project'
+
+    expect(current_path).to eq(new_project_path)
+
+    fill_in "Name", with: "New Project Name"
+    fill_in "Description", with: "This is an empty project"
+    fill_in "Target pledge amount", with: "1500"
+    fill_in "Website", with: "www.themenow"
+    select (Time.now.year - 1).to_s, :from => "project_pledging_ends_on_1i"
+
+    # If you're taking advantage of the HTML 5 date field in Chrome,
+    # you'll need to use 'fill_in' rather than 'select'
+    # fill_in "Released on", with: (Time.now.year - 1).to_s
+
+    click_button 'Create Project'
+
+    expect(current_path).to eq(project_path(Project.last))
+
+    expect(page).to have_text('New Project Name')
+  end
+end
