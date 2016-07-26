822a2b0e68e710e2dc167af4274290a983fbe1f1
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-07-26 10:58:39 +0200

    added flash scss styling, refactoring  update def controler for notice, updated layout for flash styling only after updating

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,8 +14,7 @@ class ProjectsController < ApplicationController
   def update
     @project = Project.find(params[:id])
     if @project.update(project_params)
-      flash[:notice] = "Event succesfully updated!"
-      redirect_to @project
+      redirect_to @project, notice: "Event succesfully updated!"
     else
       render :edit
     end
