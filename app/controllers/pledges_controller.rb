c0968c6df25c3c9933c2f0a2f471d82f07210a0e
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-07-27 11:45:59 +0200

    added create action for plegde form, refactoring pledge controller, updated error message partial

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,11 +1,31 @@
 class PledgesController < ApplicationController
+  before_action :set_pledge
+  
   def index
-    @project = Project.find(params[:project_id])
     @pledges = @project.pledges
   end
 
   def new
-    @project = Project.find(params[:project_id])
     @pledge = @project.pledges.new
+  end
+
+  def create
+    @pledge = @project.pledges.new(pledge_params)
+    if @pledge.save
+      redirect_to project_pledges_path(@project),
+                  notice: "Thanks, your pledge has been received!"
+    else
+      render :new
+    end
+
+  end
+
+private
+  def pledge_params
+    params.require(:pledge).permit(:name, :email, :pledge_amount)
+  end
+
+  def set_pledge
+    @project = Project.find(params[:project_id])
   end
 end
